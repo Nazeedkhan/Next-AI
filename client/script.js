a6b9615757875b34bbcbce8089731c44355e3b01
Nazeed khan <nazeedkhan10@gmail.com>
2023-03-31 03:20:28 +0530

    updated

--- client/script.js
+++ client/script.js
@@ -68,7 +68,7 @@ const handleSubmit = async (e) => {
 
   loader(messageDiv);
 
-  const response = await fetch("http://localhost:5000/", {
+  const response = await fetch("https://next-ai-icqo.onrender.com", {
     method: "POST",
     headers: {
       "Content-Type": "application/json",
